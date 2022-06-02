735c69c07dd0e802eb6240f54cfbaa3a2ed3453c
mshallow <c.shallow@wustl.edu>
2022-06-02 14:06:12 -0700

    fixing tuple indexing

--- prey_capture_python/analysis/prey_cap_metrics.py
+++ prey_capture_python/analysis/prey_cap_metrics.py
@@ -98,7 +98,7 @@ def preycap_metrics(cricket_xy, cricket_p, range, mouse_spd, az, fr=200, oldmode
         # find the first intercept for sake of calculating latency to intercept not approach
         # hope is that this will be closer to the latency to attack values used in Zhao et al 2019
         firstintercept= np.where(intercept>0)[0][0]
-        timetointercept=approachEnds[firstintercept][0]/fr  #return this
+        timetointercept=approachEnds[0][firstintercept]/fr  #return this
 
         # calculate probability of intercept given approach
         tot_approach = np.size(approachEnds)
